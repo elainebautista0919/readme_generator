9525787ca55a76cc65e90df358077d1d5b13aae8
Elaine Bautista <elainebautista@Elaines-MacBook-Pro.local>
2022-12-06 23:36:16 -0500

    Updated questions

--- index.js
+++ index.js
@@ -1,147 +1,127 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('.utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = () => {
+    return inquirer
+    .prompt([
     {
         type: 'input',
         name: 'title',
         message: 'What is your project title?',
-        validate: titleInput => {
-            if (titleInput) {
-                return true;
-            } else {
-                console.log('Please enter your project title');
-                return false;
-            }
+        validate: function(answer) {
+            if (answer) {
+            return true;
+        } else {
+            console.log('Please enter your project title')
+            return false;
         }
+      }
     },
     {
         type: 'input',
         name: 'motivation',
         message: 'What was your motivation?',
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Please enter your project motivation');
-                return false;
-            }
+        validate: function(answer) {
+            if (answer) {
+            return true;
+        } else {
+            console.log('Please enter your project motivation')
+            return false;
         }
+      }
     },
     {
         type: 'input',
         name: 'reason',
         message: 'Why did you build this project?',
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Please enter your project reason');
-                return false;
-            }
+        validate: function(answer) {
+            if (answer) {
+            return true;
+        } else {
+            console.log('Please enter your project reason')
+            return false;
         }
+      }
     },
     {
         type: 'input',
         name: 'problem',
         message: 'What problem will it solve?',
-        validate: whatInput => {
-            if (whatInput) {
-                return true;
-            } else {
-                console.log('Please enter what your project will solve');
-                return false;
-            }
+        validate: function(answer) {
+            if (answer) {
+            return true;
+        } else {
+            console.log('Please enter what your project will solve')
+            return false;
         }
+      }
     },
     {
         type: 'input',
         name: 'learn',
         message: 'What did you learn?',
-        validate: whyInput => {
-            if (whyInput) {
-                return true;
-            } else {
-                console.log('Please enter what you learned');
-                return false;
-            }
+        validate: function(answer) {
+            if (answer) {
+            return true;
+        } else {
+            console.log('Please enter what you learned')
+            return false;
         }
+      }
     },
     {
         type: 'input',
         name: 'installation',
         message: 'What are the steps to install your project?',
-        validate: installInput => {
-            if (installInput) {
-                return true;
-            } else {
-                console.log('Please enter your installation steps');
-                return false;
-            }
+        validate: function(answer) {
+            if (answer) {
+            return true;
+        } else {
+            console.log('What are the steps to install your project?')
+            return false;
         }
+      }
     },
     {
         type: 'input',
         name: 'usage',
         message: 'Please provide instructions and examples for use.',
-        validate: usageInput => {
-            if (usageInput) {
-                return true;
-            } else {
-                console.log('Please enter your instructions and examples of use');
-                return false;
-            }
+        validate: function(answer) {
+            if (answer) {
+            return true;
+        } else {
+            console.log('Please enter your instructions and examples of use')
+            return false;
         }
+      }
     },
     {
         type: 'input',
         name: 'credits',
         message: 'Please enter your collaborators.',
-        validate: usageInput => {
-            if (usageInput) {
-                return true;
-            } else {
-                console.log('Please enter your collaborators');
-                return false;
-            }
+        validate: function(answer) {
+            if (answer) {
+            return true;
+        } else {
+            console.log('Please enter your collaborators')
+            return false;
         }
+      }
     },
     {
         type: 'list',
         name: 'license',
         message: 'Please choose a license.',
-        choices: ['mit', 'apache-2.0', 'gpl-3.0', 'lgpl-3.0', 'mpl-2.0', 'none']
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?',
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Please enter your email address');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'githug',
-        message: 'What is your GitHub?',
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Please enter your GitHub username');
-                return false;
-            }
-        }
-    },
-];
+        choices: ['mit', 'apache-2.0', 'gpl-3.0', 'lgpl-3.0', 'mpl-2.0', 'none'],
+        default: 'mit'
+    }])
+    .then(input => {
+        return input;
+    })
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -152,7 +132,18 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+        .then(input => {
+            return generateMarkdown(input);
+        })
+        .then(markdown => {
+            writeToFile('./readme/README.md', markdown)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
 
 // Function call to initialize app
 init();
