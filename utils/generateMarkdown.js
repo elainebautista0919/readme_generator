83d3f1da536ab9af78c9e52948646fa025cca2a5
Elaine Bautista <elainebautista@Elaines-MacBook-Pro.local>
2022-12-06 22:31:14 -0500

    Added function for license section of ReadMe

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) { 
   if (license !== 'none') {
-    return `![License](https://img.shields.io/badge/license-${license}-blue)`;
+    return `![License Badge](https://img.shields.io/badge/license-${license}-blue)`;
   } else {
     return '';
   }
@@ -22,7 +22,17 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'none') {
+  return `## [License](#Table-of-Contents)
+
+  The project is covered by the following license:
+
+  ${renderLicenseLink(license)}`;
+  } else {
+    return 'There is no license for this project.';
+  }
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
